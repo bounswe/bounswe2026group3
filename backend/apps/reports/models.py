68050d2d5986e8ae1d386db3d224f460d4831730
Ulaş Sertan KEMEÇ <ulassertankemec@Ulas-MacBook-Air-2.local>
2026-04-06 15:22:00 +0300

    bug: update report id to be consistent, DB should update too

--- backend/apps/reports/models.py
+++ backend/apps/reports/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.conf import settings
 from django.db import models
 
@@ -39,6 +41,7 @@ class Report(models.Model):
         on_delete=models.CASCADE,
         related_name="reports",
     )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
     category = models.CharField(max_length=25, choices=ObstacleCategory.choices)
